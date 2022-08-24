826320ab9f2962c0f56a937bd3a7259d1b44f124
metype <39036543+metype@users.noreply.github.com>
2022-08-24 16:06:21 +0000

    Build: (190851d) I really did not look at this workflow

---
+++ server/chunks/app/client.manifest.mjs
@@ -0,0 +1,13 @@
+const client_manifest = {
+  "node_modules/nuxt/dist/app/entry.mjs": {
+    "file": "entry-3721c28f.mjs",
+    "src": "node_modules/nuxt/dist/app/entry.mjs",
+    "isEntry": true,
+    "css": [
+      "entry.d97e324a.css"
+    ]
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
